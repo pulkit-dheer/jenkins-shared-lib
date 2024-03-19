18ca7af6b6264f488722e7130dd543931f3c6194
pulkit <pulkitkdheer@gmail.com>
2024-03-20 00:31:32 +0530

    docker build

--- vars/dockerBuild.groovy
+++ vars/dockerBuild.groovy
@@ -1,7 +1,8 @@
-def call(String project,String ImageTag, String hubUser) {
+// Shared library code
+def dockerbuild(String project, String ImageTag, String hubUser) {
     sh """
     docker image build -t ${hubUser}/${project} .
     docker image tag ${hubUser}/${project} ${hubUser}/${project}:${ImageTag}
     docker image tag ${hubUser}/${project} ${hubUser}/${project}:latest
     """
-} 
+}
